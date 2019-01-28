3728c51f17c8130caf87206e94be52baa70b5075
哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>
2019-01-28 12:08:22 +0900

    Add files via upload

---
+++ JAPANESE.lua
@@ -0,0 +1,214 @@
+JAPANESE = {}
+
+--Translates
+
+JAPANESE["UPDATER_AVAILABLE"] =           			"の更新が有ります"
+JAPANESE["UPDATER_UPDATE"] =               			"アプリケーションの更新をしますか"
+JAPANESE["CHINESE_FONT_DOWNLOAD"] =					"中国語のフォントのダウンロード
+JAPANESE["MENU_TRANSLATE"] =							"Autopluginの言語"
+JAPANESE["TRANSLATE_ENGLISH_TITLE"] =					"English: "
+JAPANESE["TRANSLATE_CURRENT_TITLE"] =					"現在: "
+JAPANESE["TRANSLATE_YOUR_TITLE"] =					"修正後: "
+JAPANESE["MENU_TRANSLATE_DESC"] =						"言語を翻訳する事でAutopluginを改善するのを手伝って下さい"
+JAPANESE["SAVE_TRANSLATIONS"] =						"言語ファイルの保存"
+JAPANESE["EXIT_SAVE_TRANSLATIONS"] =      			"言語ファイルは保存されませんでした。終了する前に保存しますか
+JAPANESE["TRANSLATION_SAVED_1"] =						"言語ファイル保存されました 場所:"
+JAPANESE["TRANSLATION_SAVED_2"] =						"このファイルをAutoplugin GitHubにアップロードして下さい"
+JAPANESE["NOTHING_TO_TRANSLATE"] =					"関心をお寄せいただきありがとうございます、しかし今すぐ翻訳する物は何もありません"
+JAPANESE["STRING_UNSAFE_MODE"] =						"UNSAFE MODE is required for this Homebrew!"
+JAPANESE["STRING_PSVITA_RESTART"] =					"Your PSVita will restart"
+JAPANESE["STRING_BACKUP_CONFIGS"] =					"Creating Config_backups.txt"
+JAPANESE["STRING_MISSING_CONFIG"] =					"Missing config.txt"
+JAPANESE["STRING_BACK"] =								"To go back to menu"
+JAPANESE["STRING_CLOSE"] =							"Exit Autoplugin"
+JAPANESE["STRING_INSTALLED"] =						"has been installed"
+JAPANESE["STRING_PLEASE_WAIT"] =						"Please wait..."
+JAPANESE["RESTART_QUESTION"] =						"Do you want to restart now ?"
+
+--Options Menus
+JAPANESE["MENU_TITLE"] =								"Autoplugin"
+JAPANESE["MENU_PSVITA"] =								"Plugins for Vita"
+JAPANESE["MENU_PSP"] =								"Plugins for PSP (Adrenaline)"
+JAPANESE["MENU_EXTRAS"] =								"Extras"
+JAPANESE["MENU_EXIT"] =								"Exit"
+
+--Desc Options PSVITA
+JAPANESE["INSTALL_PLUGINS_DESC"] =					"Install the most useful Plugins made by SCENE-PSVITA"
+JAPANESE["UNINSTALL_PLUGINS_DESC"] =					"Uninstall plugins easily and simply"
+JAPANESE["INSTALL_SD2VITA_DESC"] =					"Install Plugin SD2VITA (StorageMgr by CelesteBlue)"
+JAPANESE["CONFIG_SD2VITA_DESC"] =						"Configure Plugin SD2VITA (StorageMgr by CelesteBlue)"
+JAPANESE["CHECK_ONLINEP_DESC"] =						"Check if there are new updated plugins"
+
+JAPANESE["MENU_TITLE_PSVITA"] =						"Vita Plugins"
+JAPANESE["MENU_INSTALL_SD2VITA"] =					"Install Plugin for SD2VITA"
+JAPANESE["MENU_CONFIGURE_SD2VITA"] =					"Configure Plugin for SD2VITA"
+JAPANESE["MENU_INSTALL_PLUGINS"] =					"Install Plugins"
+JAPANESE["MENU_UNINSTALL_PLUGINS"] =					"Uninstall Plugins"
+JAPANESE["MENU_CHECK_ONLINEP"] =						"Check for Plugin Updates"
+
+JAPANESE["MENU_TITLE_PSP"] =							"PSP (Adrenaline) Plugins"
+JAPANESE["MENU_INSTALL_NPDRMFREE"] =					"Install plugin npdrm free from qwikrazor87"
+JAPANESE["MENU_INSTALL_REMASTERED_CTRLS"] =			"Install RemasteredControls plugins from TheOfficialFloW"
+
+JAPANESE["MENU_CONVERTBOOTSPLASH"] =					"Set Custom Boot Splash"
+JAPANESE["MENU_CUSTOMWARNING"] =						"Set Custom Warning Message"
+JAPANESE["MENU_INSTALL_CONFIG"] =						"Install config.txt for PKGJ v0.45+"
+JAPANESE["MENU_INSTALL_CONFIG_DESC"] =				"Install config.txt for PKGJ (Require Wifi)"
+JAPANESE["MENU_INSTALLED_CONFIG"] =					"ux0:pkgi/config.txt for PKGJ has been installed!"
+JAPANESE["MENU_QENCORE"] =							"Install Q-Encore by Yoti"
+JAPANESE["MENU_QENCORE_DESC"] =						"Quick h-encore launcher by Yoti (Warning: h-encore must already be installed)"
+
+JAPANESE["MENU_QENCORE_ASK"] =						"Should Q-Encore be installed on"
+JAPANESE["MENU_INSTALLED_QENCORE"] =					"Q-Encore was installed on"
+JAPANESE["MENU_NOT_INSTALLED_QENCORE"] =				"Q-Encore was not installed on"
+JAPANESE["MENU_QENCORE_PATCHED"] =					"Q-Encore was patched!"
+JAPANESE["MENU_QENCORE_NOT_PATCHED"] =				"Q-Encore was not patched!"
+
+JAPANESE["MENU_QENCORE_NOGAME"] =						"H-Encore must be installed to patch with Q-Encore!"
+
+--install sd2vita
+JAPANESE["MENU_TITLE_SD2VITA"] =						"Install Storage Manager plugin for SD2VITA"
+JAPANESE["SD2VITA_CONFIG_TITLE"] =					"Configure Storage Manager plugin for SD2VITA"
+JAPANESE["SD2VITA_CONFIG_DEVICE"] =					"Device"
+JAPANESE["SD2VITA_CONFIG_MOUNT"] =					"Mount Point"
+JAPANESE["SD2VITA_CONFIG_CHANGE"] =					"Change Mount Point"
+JAPANESE["SD2VITA_CONFIG_CANCEL"] =					"Cancel"
+JAPANESE["SD2VITA_CONFIG_SAVE"] =						"Save Current Configuration"
+JAPANESE["SD2VITA_1_WARNING"] =						"To install Storage Manager plugin Your sd2vita MUST be ready"
+JAPANESE["SD2VITA_2_WARNING"] =						"If a gamecard is inserted it MUST be removed before rebooting!"
+JAPANESE["SD2VITA_3_WARNING"] =						"VERY IMPORTANT: your sd2vita MUST have some apps already in it, you can copy/paste"
+JAPANESE["SD2VITA_4_WARNING"] =						"them from your official MC only copy really needed apps to your sd."
+JAPANESE["SD2VITA_5_WARNING"] =						"For example: H-encore, Vitashell/ONEMenu, PKGJ, adrenaline, etc... "
+JAPANESE["SD2VITA_GAMECARD_DETECTED"] =				"GAMECARD DETECTED!"
+JAPANESE["SD2VITA_GAMECARD_REMOVED"] =				"Remove before install"
+JAPANESE["SD2VITA_INSTALL"] =							"To proceed with the installation"
+JAPANESE["SD2VITA_CANCEL"] =							"To cancel the installation"
+JAPANESE["SD2VITA_INVALID_CONFIG"] =					"                                 Invalid config!\n\nEnsure that all mount points are unique or set to none\n                                and ux0 is set."
+JAPANESE["SD2VITA_UPDATED_CONFIG"] =					"Storage Manager Config Updated!"
+JAPANESE["MOUNT_NONE_FRIENDLY"] =						"Unmounted"
+JAPANESE["MOUNT_UX0_FRIENDLY"] =						"LiveArea Applications"
+JAPANESE["MOUNT_XMC0_FRIENDLY"] =						"Unused"
+JAPANESE["MOUNT_IMC0_FRIENDLY"] =						"Internal Memory on Vita 2K/PSTV"
+JAPANESE["MOUNT_UMA0_FRIENDLY"] =						"USB Port"
+JAPANESE["MOUNT_GRW0_FRIENDLY"] =						"Game Card Slot"
+JAPANESE["SD2VITA_MCD_FRIENDLY"] =					"Official Memory Card"
+JAPANESE["SD2VITA_INT_FRIENDLY"] =					"Internal Memory on Vita 2K/PSTV"
+JAPANESE["SD2VITA_GCD_FRIENDLY"] =					"SD2VITA"
+JAPANESE["SD2VITA_UMA_FRIENDLY"] =					"USB or PSVSD on Vita 3G"
+
+--install plugins
+JAPANESE["INSTALLP_LIST_PLUGINS"] =					"List of plugins ready to install:"
+JAPANESE["INSTALLP_MARK"] =							"Mark/Unmark Plugin(s)"
+JAPANESE["INSTALLP_CONFIRM"] =						"Install Plugin(s)"
+JAPANESE["INSTALLP_ALL"] =							"To Install ALL the plugins"
+JAPANESE["INSTALLP_CLEAN"] =							"To clean selected plugins"
+JAPANESE["INSTALLP_LR_SWAP"] =						"To swap ux0/ur0"
+JAPANESE["INSTALLP_CUSTOM_PATH"] =					"Install in the custom path"
+
+JAPANESE["INSTALLP_VITABRIGHT_360"] =					"Vitabright is only for 3.60/H-encore 3.68"
+JAPANESE["INSTALLP_CWARNING_360_365"] =				"Custom Warning doesn't work on 3.67/3.68"
+JAPANESE["INSTALLP_QUESTION_SHELLSECBAT"] =			"\n               Plugin shellsecbat detected!\n\nInstall shellbat instead and uninstall shellsecbat ?"
+JAPANESE["INSTALLP_QUESTION_SHELLBAT"] =				"\n               Plugin shellbat detected!\n\nInstall shellsecbat instead and uninstall shellbat ?"
+
+JAPANESE["INSTALLP_WARNING_REFOOD"] =					"Plugin Refood to work will be installed on ur0:/tai"
+JAPANESE["INSTALLP_OSK_TITLE"] =						"Text for Custom Warning"
+JAPANESE["INSTALLP_OSK_TEXT"] =						"Put text here"
+
+--Uninstall plugins
+JAPANESE["UNINSTALLP_TITLE"] =						"Plugins Manager"
+JAPANESE["UNINSTALLP_NOCONFIG_FOUND"] =				"No config.txt found at: "
+JAPANESE["UNINSTALLP_PLUGIN"] =						"To uninstall plugin"
+JAPANESE["UNINSTALLP_LEFTRIGHT_SECTION"] =			"<--/--> To switch section"
+
+--npdrm free
+JAPANESE["NPDRMFREE_TITLE"] =							"Install NPDRM FREE plugin from Qwik"
+JAPANESE["NPDRMFREE_INSTALLED"] =						"npdrm free installed"
+JAPANESE["NPDRMFREE_VSH_UPDATED"] =					"vsh.txt updated"
+JAPANESE["NPDRMFREE_GAME_UPDATED"] =					"game.txt updated"
+JAPANESE["NPDRMFREE_INSTALL"] =						"Install npdrm free plugin"
+JAPANESE["NPDRMFREE_DESC"] =							"A PSP/ePSP plugin to run official NPDRM PS1/PSP content without need of a valid .rif license or act.dat."
+
+--customWaning
+JAPANESE["CUSTOMWARNING_MESSAGE"] =					"Remember to enable the Custom Warning plugin"
+
+--customsplash
+JAPANESE["CUSTOMBOOTSPLASH__TITLE"] =					"Custom Boot Splash"
+JAPANESE["CUSTOMBOOTSPLASH_CONVERT"] =				"To convert image"
+JAPANESE["CUSTOMBOOTSPLASH_IMPORT"] =					"Import a image from your Gallery (only PNG)"
+JAPANESE["CUSTOMBOOTSPLASH__MESSAGE"] =				"Remember to enable the Custom Boot Splash plugin"
+JAPANESE["CUSTOMBOOTSPLASH_NOPNG_FOUND"] =			"ux0:CustomBootsplash EMPTY! "
+JAPANESE["CUSTOMBOOTSPLASH_NOPNG"] =					"Format Compatible PNG"
+
+--pspctrls
+JAPANESE["PSPCTRLS_TITLE"] =							"Remastered Controls plugins from TheOfficialFloW (Adrenaline)"
+JAPANESE["PSPCTRLS_GAME_UPDATED"] =					"game.txt updated"
+JAPANESE["PSPCTRLS_DESC"] =							"A collection of dual analog patches for PSP games on Adrenaline"
+JAPANESE["PSPCTRLS_LR_SWAP"] =						"To swap partitions"
+
+--plugins online
+JAPANESE["UPDATE_WIFI_LOW"] =							"Wifi Low Signal"
+JAPANESE["UPDATE_WIFI_IS_ON"] =						"Internet is required"
+JAPANESE["UPDATE_CHECK_DB"] =							"Checking Database"
+JAPANESE["UPDATE_PLUGIN"] =							"Updated Plugin"
+JAPANESE["UPDATE_ERROR_DB"] =							"Error downloading db.lua"
+JAPANESE["UPDATE_NO_NEWS"] =							"All the plugins are up to date"
+JAPANESE["UPDATE_VITACHEAT_DB"] =						"Update Cheats db for Vitacheat?"
+JAPANESE["UPDATE_VITACHEAT_DB_WRITE"] =				"The files will be overwritten!"
+JAPANESE["UPDATE_CHECK_VITACHEAT_DB"] =				"Checking Vitacheat Database"
+
+--descriptions plugins
+--KERNEL
+JAPANESE["INSTALLP_DESC_NONPDRM"] =					"Allows you to bypass DRM protection on any PS Vita content"
+JAPANESE["INSTALLP_DESC_MINIVITATV"] =				"Allows you to play local multiplayer games on your PS Vita with up to 4 friends"
+JAPANESE["INSTALLP_DESC_NOPSMDRM"] =					"Allows you to bypass DRM protection on PSM game content"
+JAPANESE["INSTALLP_DESC_VITABRIGHT"] =				"Enables you to use a custom gamma table, and thus decrease brightness below the minimum, and increase it above the maximum"
+JAPANESE["INSTALLP_DESC_NOAVLS"] =					"Disables AVLS on AVLS-Forced vitas"
+JAPANESE["INSTALLP_DESC_REPATCH"] =					"Allows modding of encrypted games (similar to ux0:patch) and enables game patching to bypass minimum firmware requirement"
+JAPANESE["INSTALLP_DESC_LOLICON"] =					"Enables actual overclocking via a small Vita shell ran from kernel"
+JAPANESE["INSTALLP_DESC_DS3VITA"] =					"Control your Vita with a DualShock 3"
+JAPANESE["INSTALLP_DESC_DS4VITA"] =					"Control your Vita with a DualShock 4"
+JAPANESE["INSTALLP_DESC_UDCDUVC"] =					"Enables you to stream your Vita screen to your PC"
+JAPANESE["INSTALLP_DESC_VIIMOTE"] =					"Control your Vita with a WiiMote"
+JAPANESE["INSTALLP_DESC_KUIO"] =						"Allows user modules to access ux0:data for basic I/O operations. May be necessary for certain specific plugins"
+JAPANESE["INSTALLP_DESC_ADRENALINE"] =				"Allows you to run adrenaline with a single touch"
+JAPANESE["INSTALLP_DESC_DSMOTION"] = 					"DualShock motion control support for PlayStation TV"
+JAPANESE["INSTALLP_DESC_USBMC"] = 					"USB storage as Vita memory card"
+JAPANESE["INSTALLP_DESC_ANALOGSENHANCER"] =			"This little kernel plugin allows users to improve their experience with PSVITA analogs"
+JAPANESE["INSTALLP_DESC_IOPLUS"] = 					"A PSVita kernel plugin that allows more IO operations in userland"
+JAPANESE["INSTALLP_DESC_VITACHEAT"] =					"Use Cheats On PSVita Games"
+JAPANESE["INSTALLP_DESC_NOPSMWHITE"] =				"Disables PSTV Whitelist for PSM Games"
+JAPANESE["INSTALLP_DESC_ANALOGSTICK"] =				"Plugin to completely disable PSVita's analog stick"
+JAPANESE["INSTALLP_DESC_CIDSPOOF"] =					"Spoof CID (ConsoleID) on PSVITA(Could possibly be used to unban your console from PSN)"
+JAPANESE["INSTALLP_DESC_MAKEPSM"] =					"Modifications to the PSM Developer applications"
+JAPANESE["INSTALLP_DESC_REFOOD"] =					"A vita plugin to run programs that do not get decrypted properly."
+
+--Boot_config.txt
+JAPANESE["INSTALLP_DESC_CUSTOMBOOTSPLASH"] =			"This plugin can display Boot Splash on PS Vita/PSTV at boot. Requires ENSO."
+JAPANESE["INSTALLP_DESC_CONVERTBOOTSPLASH"] =			"Set your splash image as custom boot splash ?"
+JAPANESE["INSTALLP_DESC_NOFINDSPLASH"] =				"Place the image of your choice at ux0:CustomBootsplash/\n(renamed to splash.png and resized to 960x544)"
+JAPANESE["INSTALLP_DESC_SPLASHGH"] =					"The size of the image should be: 960 * 544"
+JAPANESE["INSTALLP_DESC_BOOTSPLASHDONE"] =			"boot_splash.bin DONE !!!"
+
+--MAIN
+JAPANESE["INSTALLP_DESC_DENABLER"] =					"Download files through your browser"
+JAPANESE["INSTALLP_DESC_NOLOCKSCREEN"] =				"Disables the lockscreen found on the PSVita"
+JAPANESE["INSTALLP_DESC_NOTROPHYMSG"] =				"Disables the trophy hint message when you launch application whose savedatas are linked to a different account. Useful if you constantly switch accounts"
+JAPANESE["INSTALLP_DESC_CUSTOMWARNING"] =				"Allows you to set a custom warning message instead of the epilepsy warning when you bootup with ENSO"
+JAPANESE["INSTALLP_DESC_SHELLBAT"] =					"Battery percent in statusbar"
+JAPANESE["INSTALLP_DESC_SHELLSECBAT"] =				"Battery percent in statusbar: ShellBat and LastSeconds fusion"
+JAPANESE["INSTALLP_DESC_PNGSHOT"] =					"pngshot is a plugin to make screenshots great again (.PNG)"
+JAPANESE["INSTALLP_DESC_VFLUX"] =						"Screen filter plugin for PS Vita"
+JAPANESE["INSTALLP_DESC_RESKYPE"] =					"Just a simple plugin to enable the Skype app on 3.65+"
+JAPANESE["INSTALLP_DESC_AUTOBOOT"] =					"This plugin simply auto-launch an application right after enso startup like AutoStart used to do on PSP"
+
+--ALL
+JAPANESE["INSTALLP_DESC_VSHMENU"] =					"A VSH menu taiHEN plugin for the PS VITA games and homebrews"
+JAPANESE["INSTALLP_DESC_VITAGRAFIX"] =				"Plugin that allows you to change resolution and FPS cap of PS Vita games"
+JAPANESE["INSTALLP_DESC_OVERCLOCK"] =					"Simple Overclock Plugin for Taihen"
+JAPANESE["INSTALLP_DESC_FRAMECOUNTER"] =				"This is a little plugin that works as an FPS counter. It will show an FPS counter on the top left side of your screen."
+JAPANESE["INSTALLP_DESC_VGI"] =						"A simple taihen plugin that prints useful information about PS Vita games."
+JAPANESE["INSTALLP_DESC_FUCKPSSE"] =					"A plugin to decrypt PSM games"
+JAPANESE["INSTALLP_DESC_PSMPATCH"] =					"Allows you to modify PSM Games, and also can be used to load PSM Homebrew."
+JAPANESE["INSTALLP_DESC_ITLSENSO"] =					"Adds TLS v1.2 to Enso enabled devices(requires vpk of the application)."
+JAPANESE["INSTALLP_DESC_TROPHAX"] =					"Universal trophy unlocker for PSVITA."
+JAPANESE["INSTALLP_DESC_TURBOPAD"] =					"This plugin allows you to enable RapidFire on whatever button."

167448d73be8b6569a882c550db221533e35bf01
哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>
2019-11-17 08:27:46 +0900

    4.06

--- ENGLISH_US.lua
+++ ENGLISH_US.lua
@@ -204,6 +204,7 @@ ENGLISH_US["INSTALLP_DESC_NOPSMDRM"] =					"Allows you to bypass DRM protection 
 ENGLISH_US["INSTALLP_DESC_VITABRIGHT"] =				"Enables you to use a custom gamma table, and thus decrease brightness below the minimum, and increase it above the maximum"
 ENGLISH_US["INSTALLP_DESC_NOAVLS"] =					"Disables AVLS on AVLS-Forced vitas"
 ENGLISH_US["INSTALLP_DESC_REPATCH"] =					"Allows modding of encrypted games (similar to ux0:patch) and enables game patching to bypass minimum firmware requirement"
+ENGLISH_US["INSTALLP_DESC_REPATCH271"] =				"Antecedent version of rePATCH which solves some problems with version 3.0"
 ENGLISH_US["INSTALLP_DESC_LOLICON"] =					"Enables actual overclocking via a small Vita shell ran from kernel"
 ENGLISH_US["INSTALLP_DESC_DS3VITA"] =					"Control your Vita with a DualShock 3"
 ENGLISH_US["INSTALLP_DESC_DS4VITA"] =					"Control your Vita with a DualShock 4"
